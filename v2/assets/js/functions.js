1f687ad5c1527cc04fd64f49438c8746532c674a
Lucas Bonomi <lucas.bonomi@gmail.com>
2014-06-08 23:48:15 +0200

    update repo

---
+++ v2/assets/js/functions.js
@@ -0,0 +1,21 @@
+$(function(){
+
+
+function sidebar(){
+  var trigger = $('.menusign, #close'),
+  menu = $('.sidebar');
+
+  trigger.on('click',function(){
+   menu.toggleClass('closed');
+  });
+}
+
+
+
+ function deploy(){
+  sidebar();
+ }
+
+ deploy();
+
+}).call($(this));
